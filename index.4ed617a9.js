c30104a4f473394420aa14773c110d75623121c9
OlexandrBilyk <147237948+OlexandrBilyk@users.noreply.github.com>
2024-10-29 21:26:53 +0000

    Deploying to gh-pages from @ OlexandrBilyk/Chocolate@ad49c7ca28a23e75e98cbe2671facfa861c9a54a 🚀

---
+++ index.4ed617a9.js
@@ -0,0 +1,2 @@
+var topScroll=document.querySelector(".scroll-top");function toggleScroll(){document.body.scrollTop>200||document.documentElement.scrollTop>200?topScroll.classList.remove("hidden"):topScroll.classList.add("hidden")}window.onscroll=function(){toggleScroll()};
+//# sourceMappingURL=index.4ed617a9.js.map
